8205a50452c1154df0bd003eeb4e74f96d4a5d58
np6176a <niyamp3@gmail.com>
2015-02-25 23:52:20 -0500

    minifying files

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@
  */
 var gulp = require('gulp');
 var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
+gulp.task('minify-js', function() {
+    gulp.src('./dev/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./template/js/'))
+});
 
 gulp.task('minify-css', function() {
-    return gulp.src('./static/css/*.css')
+    return gulp.src('./dev/Styles/*.css')
         .pipe(minifyCSS({keepBreaks:true}))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./template/Styles/'))
 });
