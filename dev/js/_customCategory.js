fba2eb777bd48513d8bf641a7da9a3ca4723f574
np6176a <niyamp3@gmail.com>
2015-03-20 14:15:16 -0400

    filter button default

--- dev/js/_customCategory.js
+++ dev/js/_customCategory.js
@@ -1,1 +1,7 @@
-$(document).ready(function(){$(".slideout-menu-toggle").on("click",function(e){e.preventDefault();var t=$(".slideout-menu"),n=$(".slideout-menu").width();t.toggleClass("open"),t.hasClass("open")?t.animate({left:"0px"}):t.animate({left:-n},250)})});
+$(document).ready(function () {
+    $(".slideout-menu-toggle").on("click", function (e) {
+        e.preventDefault();
+        var t = $(".slideout-menu"), n = $(".slideout-menu").width();
+        t.toggleClass("open"), t.hasClass("open") ? t.animate({left: "0px"}) : t.animate({left: -n}, 250)
+    })
+});
