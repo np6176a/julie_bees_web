6dcdb11bf700c96bae95e74ecd73b01d53ea3c08
np6176a <niyamp3@gmail.com>
2015-03-21 15:11:01 -0400

    js.

--- dev/js/_customCategory.js
+++ dev/js/_customCategory.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
-    $(".slideout-menu-toggle").on("click", function (e) {
-        e.preventDefault();
-        var t = $(".slideout-menu"), n = $(".slideout-menu").width();
-        t.toggleClass("open"), t.hasClass("open") ? t.animate({left: "0px"}) : t.animate({left: -n}, 250)
+    $(".slideout-menu-toggle").on("click", function (event) {
+        event.preventDefault();
+        var slideout_menu = $(".slideout-menu");
+        var menu_width = $(".slideout-menu").width();
+        slideout_menu.toggleClass("open");
+
+        if (slideout_menu.hasClass("open")) {
+            slideout_menu.animate({left: "0px"});
+        } else {
+            slideout_menu.animate({left: -menu_width}, 250);
+        }
     })
 });
